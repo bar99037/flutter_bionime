4940649ad5bd04d474c67ee05c4539d061a69588
bar99037 <bar99037@gmail.com>
2021-03-16 23:31:25 +0800

    多國語系實作

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bionime/model/Locales.dart';
 import 'package:flutter_bionime/pages/home_page.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(EasyLocalization(
+    supportedLocales: languages.map<Locale>((e) => e.locale).toList(),
+    fallbackLocale: languages.first.locale,
+    path: 'resources/langs',
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter BIONIME TEST',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
+    return ScreenUtilInit(
+      designSize: Size(600, 800),
+      allowFontScaling: false,
+      builder: () => MaterialApp(
+        title: 'Flutter BIONIME TEST',
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage(),
+      )
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,47 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_bionime/model/Language.dart';
+import 'package:flutter_bionime/model/Locales.dart';
 
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(padding: EdgeInsets.all(8.w),
+              child: DropdownButton<Language>(
+                icon: Icon(
+                    Icons.language,
+                    color: Colors.white
+                ),
+                onChanged: (Language language){
+                  context.setLocale(language.locale);
+                },
+                items: languages.map<DropdownMenuItem<Language>>(
+                        (e) => DropdownMenuItem<Language>(
+                      value: e,
+                      child: Text(
+                          e.name,
+                          style: TextStyle(fontSize: 20.sp)
+                      ),
+                    )
+                ).toList(),
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
   }
 
 }
